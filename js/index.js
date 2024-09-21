2141d2622aa95e6c24f02cfa82c4fe37c57f1a22
Srinidhi Hegde <srihegde@vpn-065.umiacs.umd.edu>
2024-09-21 12:53:35 -0400

    Added mobile support for navbar and section/container updates

--- js/index.js
+++ js/index.js
@@ -8,4 +8,10 @@ $(document).ready(function() {
         $(this).find('video').css('display', 'none');
         $(this).find('img').css('display', 'inline-block');
     });
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
 })
